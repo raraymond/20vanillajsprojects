1d61fccacdd7ded8e092d2b37f3d030ca7e970fd
Ryan Raymond <ryan@Ryans-iMac.local>
2020-05-16 12:39:38 -0400

    built basic array method webpage and added add user functionality

---
+++ 05_array_methods/script.js
@@ -0,0 +1,59 @@
+const main = document.getElementById('main')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionairesBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calcWealthBtn = document.getElementById('calculate-weath')
+
+
+let data = [];
+
+
+//fetch randome use and add money
+const getRandomUser = async() => {
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json();
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+    console.log(newUser)
+    addData(newUser);
+}
+
+const addData = (obj) => {
+    data.push(obj);
+    updateDom();
+}
+
+//update DOM
+const updateDom = (providedData = data) => {
+    //clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> $${formatMoney(item.money)}`;
+        main.appendChild(element)
+    });
+}
+
+//format number as money
+const formatMoney = (number) => {
+    return (number).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+
+}
+
+
+
+//event listeners
+addUserBtn.addEventListener('click', getRandomUser)
+
+
+//initialize 3 random users
+getRandomUser();
+getRandomUser();
+getRandomUser();
